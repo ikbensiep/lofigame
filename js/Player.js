8dc3896618242cb57df038dab75b15c42ffa60ce
ikbensiep <ikbensiep>
2023-10-01 22:22:30 +0200

    Camera smoothing, tire track emitter, heli motion

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,5 @@
+import Emitter from './Emitter.js';
+
 export default class Player {
   
   constructor(game) {
@@ -8,7 +10,16 @@ export default class Player {
     
     this.width = 230;
     this.height = 120;
+    
+    this.position = {}
+    this.cameraPosition = {}
 
+    this.tireTrackPool = [];
+    this.maxTireTracks = 200;
+    this.createTireTracks();
+
+    this.currentPath = 0;
+    this.waypointsCompleted = false;
     this.paths = [
       {name:'garagebox', completed: false, points: []},
       {name:'pitbox', completed: false, points: []},
@@ -16,44 +27,65 @@ export default class Player {
       {name:'racetrack', completed: false, points:  []}
     ];
 
-    this.currentPath = 0;
-    this.waypointsCompleted = false;
 
     // Car Physics
+    // move to config system (/ api?)
     this.velocity = 50
     this.displayVelocity= 0
     this.forceForward = 0
     this.forceBackward = 0
     this.facingAngle = 0
-    this.isReversing = false
-    this.isBraking = false
+    
     this.baseForce = .50
-    this.baseTurningSpeed = 2
+    this.baseTurningSpeed = 1.5
     this.baseRoadAttrition = 0.99
     this.baseDirtAttrition = 0.8
 
     this.maxSpeedFront = 75
     this.maxSpeedBack = -3
-    this.maxTurnSpeed = 1
+    this.maxTurnSpeed = 3
 
-    this.isOnRoad = true;
+    this.isOnRoad = true
+    this.isReversing = false
+    this.isBraking = false
     
+
+    // fixme
     this.hudWaypoints = this.game.hud.querySelector('.points');
     this.hudWaypointsCollected = this.game.hud.querySelector('strong');
     this.hudWaypointsTotal = this.game.hud.querySelector('span');
-    
 
+    // emitter
+    // (Explosion is a general purpose Emitter instance in the main game object)
     this.pop = this.game.getExplosion();
     this.colliding = false;
+
+
     
-    this.position = {x: 1920, y:1080}
 
     this.init()
   }
 
+
+  createTireTracks () {
+  
+    for(let i=0; i<this.maxTireTracks; i++) {
+      this.tireTrackPool.push(new Emitter(this.game, window.rubberTrackSprite, 128, 93, 1));
+    }
+  }
+
+  getTireTrack () {
+    for(let i=0; i< this.tireTrackPool.length; i++) {
+      if (this.tireTrackPool[i].free) {
+        return this.tireTrackPool[i];
+      }
+    }
+  }
+
   init() {
     
-    if(!this.game.scene) {
+
+    if(!this.game.scene || this.game.scene == '') {
       console.log('no game scene selected!')
       return false;
     }
@@ -67,14 +99,13 @@ export default class Player {
       this.findPathWaypoints( path.name )
     });
 
-    // choose next waypoint
+    // choose first path, find set of waypoints
+    
     this.currentPath = 0;
     this.findNextWayPoint(this.currentPath);
-    console.log(this.paths)
-    
+
 
     /* Mobile input (experimental LMAO)
-
     window.addEventListener("devicemotion", (event) => {
       window.debug.textContent = event.rotationRate.alpha.toFixed(2) || '0.00';
       
@@ -125,6 +156,13 @@ export default class Player {
       const points = Math.floor(path.getTotalLength());
 
       for(let i=0; i<Math.floor(points / stepSize); i++) {
+
+        // Collision detection calculates circles. 
+        // 
+        // Instead of supplying a `radius` to collidable entities
+        // I'm using the `height` property which -probably-
+        // will exist on most game entities.
+
         let pathWaypoint = {
           x: path.getPointAtLength(i * stepSize).x,
           y: path.getPointAtLength(i * stepSize).y,
@@ -134,16 +172,19 @@ export default class Player {
       }
     }
 
-    // render active pathWaypoints to screen
+    // render pathWaypoints to screen
     const b = document.createElement('b');
     pathWaypoints.map( (waypoint, index) => {
+      
       let el = b.cloneNode();
       el.innerHTML = '&times;';
       el.className = `waypoint ${pathType}`;
+      
       el.style.left = `${Math.round(waypoint.x )}px`;
       el.style.top = `${Math.round(waypoint.y )}px`;
-      el.style.setProperty('--size', waypoint.height + 'px');
+      el.style.setProperty('--size', waypoint.height + 'px'); //css uses --size variable to set width & height on waypoints
       
+      // jaa lache
       waifupoints.appendChild(el);
       pathWaypoints[index].element = el;
     });
@@ -156,7 +197,7 @@ export default class Player {
 
   findNextWayPoint() {
   
-    console.log('current path:', this.paths[this.currentPath]);
+    console.log(`path[${this.currentPath}]`, this.paths[this.currentPath].points.length);
 
     if(!this.paths[this.currentPath].completed && this.paths[this.currentPath].points.length) {
       
@@ -165,23 +206,11 @@ export default class Player {
         y: this.paths[this.currentPath].points[0].y
       }
 
+      this.cameraPosition = {...this.position};
+
     } else {
       this.currentPath++;
     }
-    
-
-    // let currentWaypoint = this.paths.filter (waypoint => waypoint.points.length > 0)[0];
-    // if (!currentWaypoint) {
-    //   console.error('hu')
-    //   this.position = {x: 0, y: 0};
-    // } else {
-    //   // init player on the first avialable waypoint
-    //   this.position = {
-    //     x: currentWaypoint.points[0].x, 
-    //     y: currentWaypoint.points[0].y
-    //   }
-    // }
-
   }
 
   // FIXME: bad function name
@@ -219,7 +248,6 @@ export default class Player {
     });
 
 
-    
     window.debug.textContent = `Go to ${this.paths[this.currentPath].name}`;
     this.hudWaypointsTotal.textContent = this.paths[this.currentPath].points.length;
     this.hudWaypointsCollected.textContent = wphits;
@@ -258,43 +286,87 @@ export default class Player {
     this.position.y += this.velocity * Math.sin(this.facingAngle * Math.PI / 180);
     this.displayVelocity = Math.abs(Math.round(this.velocity*3) )
     
-    this.game.camera.scrollTo((this.position.x) - window.innerWidth / 2, (this.position.y) - window.innerHeight / 2)
+    // in this case the container element #camera simply scrolls.
+    // as in literal browser scrollbars.
+    //
+    // IDEA: experiment swapping this out and moving the .map child element
+    // using translate3d()
     
+    const cameraLerpSpeed = 0.22;
+    const cameraTargetX = this.position.x;
+    const cameraTargetY = this.position.y;
+    
+    this.cameraPosition.x = this.game.lerp (this.cameraPosition.x, cameraTargetX, cameraLerpSpeed);
+    this.cameraPosition.y = this.game.lerp (this.cameraPosition.y, cameraTargetY, cameraLerpSpeed);
+
+    this.game.camera.scrollTo(
+      parseInt((this.cameraPosition.x) - window.innerWidth / 2), 
+      parseInt((this.cameraPosition.y) - window.innerHeight / 2)
+    )
+    
+    this.element.style.setProperty('--x', parseInt(this.position.x));
+    this.element.style.setProperty('--y', parseInt(this.position.y));
     this.element.style.setProperty('--angle', `${this.facingAngle}deg`)
     
     this.isBraking ? this.element.classList.add('braking') : this.element.classList.remove('braking');
+
+
+
+    // Just some dumb helicopter stuff
+    let heli = window.helicopter;
+    let cx = parseInt(heli.style.left);
+    let cy = parseInt(heli.style.top);
+    let dx = parseInt(this.position.x - cx) * .5;
+    let dy = parseInt(this.position.y - cy) * .5;
+
+    const distance = parseInt(Math.hypot(dx, dy));
+    // const angleRads = Math.atan2(dy, dx);
+    const angleDegs = Math.atan2(dy, dx) * 180 / Math.PI;
+
+    const volume = (100 - ( Math.abs(distance) / 100)) / 100 ;
+    
+    if( volume < 1 && volume > 0) {
+      heli.querySelector('audio').volume = volume.toFixed(1);
+    } else {
+      // instead of play/pausing and potentially hear a little clipping everytime an audo
+      // stream is started, just play at very low volume.
+      heli.querySelector('audio').volume = 0.05;
+    }
+    
+    heli.style.setProperty('--x', (cx + dx));
+    heli.style.setProperty('--y', (cy + dy));
+    heli.style.setProperty('--rot', angleDegs);
+
+
   }
 
   honk () {
     this.carBody.querySelector('audio.horn').play()
   }
 
-
-  update (input) {
+  update (input, deltaTime) {
     // handle button input
     if(input) {
-
-      // player honking
-      if(input.includes("Shift") || input.includes("CapsLock") ){
-        this.honk();
-      }
-
+      
+      let mod = this.isReversing ? -1 : 1; 
+      
       // player input handling
       if(input.includes("ArrowRight")){
         if(this.velocity != 0){
-            this.facingAngle += this.baseTurningSpeed
+            this.facingAngle += this.baseTurningSpeed * mod
           }
       }
           
       if(input.includes("ArrowLeft")){
           if(this.velocity != 0){
-              this.facingAngle -= this.baseTurningSpeed
+              this.facingAngle -= this.baseTurningSpeed * mod
           }
       }
 
       if(input.includes("ArrowUp") || input.includes('a')){
           if(this.velocity < this.maxSpeedFront){
               this.forceForward += this.baseForce;
+              this.isReversing = false;
           }
       }
 
@@ -302,16 +374,32 @@ export default class Player {
         
         if(this.velocity > 0) {
           this.isBraking = true;
+          this.isReversing = false;
         } else {
           this.isBraking = false;
+          this.isReversing = true;
         }
 
         if(this.velocity > this.maxSpeedBack){
           this.forceBackward += this.baseForce;
         }
         
+        if (this.isBraking && this.velocity > this.maxSpeedFront * .75 ) {
+          // use deltaTime to periodically drop / clear up old tire tracks?
+          // current limit is 2000 (which will not suffice and eventually 
+          // `tiretrack` will become undefined because this code will 
+          // exceed tireTrackPool size.
+          let tiretrack = this.getTireTrack();
+          tiretrack.start(this.position.x, this.position.y, this.facingAngle );
+        }
+
       } else {
         this.isBraking = false;
+      }
+
+      // player honking
+      if(input.includes("Shift") || input.includes("CapsLock") ){
+        this.honk();
       }
     }
 
@@ -323,7 +411,7 @@ export default class Player {
       if(this.paths[this.paths.length - 1].completed) {
         this.allPathsCompleted = true;
         if(this.allPathsCompleted) {
-          window.debug.textContent = `Have fun.`;
+          window.debug.textContent = `All done! Have fun :)`;
           window.waifupoints.innerHTML = '';
           this.honk()
         }
--- js/main.js
+++ js/main.js
@@ -12,13 +12,15 @@ export default class Game {
     // simply set  .camera to overflow: hidden and no one will ever know
 
     this.loading = true;
-    this.camera = window.camera;
+    this.camera = window.camera; // this mayyy be considered bad practive but I love that any #id in an html doc can be called this way.
+    this.mouse = {x:0, y:0, height: 5};
+    
     this.hud = document.querySelector('header');
     this.animationTimer = 0;
     this.animationInterval = 1000/70;
-
     this.fpsCounter = window.fpsCounter;
-    this.worldmap = window.map;
+
+    this.worldmap = window.map; // ya this is probably _super_ bad.
     this.mapLayers = [{type:'world'},{type:'track'},{type:'elevated'}];
     
     this.scene = 'home';
@@ -26,30 +28,27 @@ export default class Game {
 
     this.explosionPool = [];
     this.maxExplosions = 20;
-    
-    this.mouse = {x:0, y:0, height: 5};
-
-
     this.createExplosionPool();
 
     this.loadScene(this.scene);
-    
-    
+
     this.player = new Player(this);
     this.input = new InputHandler(this);
     
-    
+    // in lieu of a decent scene switching system, this works for now.
     this.sceneSelector.addEventListener('change', e => {
-       this.loadScene(e.target.value);
-       
+
+      this.loadScene(e.target.value);
+       // emptying the player's current paths
+       // paths are found by the Player object when loadScene calls Player.init()
        this.player.paths.map (path => path.points.length = 0);
-       e.target.blur();
+       
+       e.target.blur(); //super annoying when you're about to drive but you switch dimensions to another world instead
     });
   }
 
   
   loadScene(worldname) {
-    
 
     this.loading = true;
     iframe.src = `./assets/track/${worldname}.svg`;
@@ -70,7 +69,7 @@ export default class Game {
       try {
 
         worldlayers.map ( (worldlayer, index) => {
-          let layerElem = this.worldmap.querySelector(`img.${worldlayer.type}`);
+          let layerElem = this.worldmap.querySelector(`.${worldlayer.type} img`);
           
           layerElem.src = `./assets/track/${worldname}.svg#${worldlayer.type}`;
           
@@ -101,24 +100,24 @@ export default class Game {
   render(deltaTime) {
     if(this.animationTimer > this.animationInterval) {
 
-      this.player.update(this.input.keys);
+      const fps = parseInt(1000/deltaTime);
+      this.fpsCounter.value = fps || 0;
+      this.fpsCounter.textContent = `${fps} fps`;
+      this.fpsCounter.nextSibling.textContent = `${fps} fps`
+
+      this.player.update(this.input.keys, deltaTime);
 
       this.explosionPool.forEach(explosion => {
+        
         explosion.update(deltaTime);
-        // update HUD
-        const fps = parseInt(1000/deltaTime);
-        this.fpsCounter.value = fps || 0;
-        this.fpsCounter.textContent = `${fps} fps`;
-        this.fpsCounter.nextSibling.textContent = `${fps} fps`
       })
 
     } else {
       this.animationTimer += deltaTime;
     }
 
-
     if(this.loading) {
-      document.body.classList.add('loading')
+      document.body.classList.add('loading') // body.loading changes the mouse cursor to `wait`; at this stage we're not building fancy UIs just yet
     } else {
       document.body.classList.remove('loading')
     }
@@ -126,16 +125,23 @@ export default class Game {
   
   checkCollision (a, b) {
 
-    let xPosA = a.position ?  a.position.x : a.x;
-    let yPosA = a.position ?  a.position.y : a.y;
+    let xPosA = a.position ? a.position.x : a.x;
+    let yPosA = a.position ? a.position.y : a.y;
     let xPosB = b.position ? b.position.x : b.x;
     let yPosB = b.position ? b.position.y : b.y;
 
+    // Since most of the items in the game will be rectangles
+    // i've opted to simply divide an item's height in half rather
+    // than adding a radius property
     const sumOfRadii = a.height / 2 + b.height/2;
     const dx = xPosA - xPosB;
     const dy = yPosA - yPosB;
     const distance = Math.hypot(dx, dy);
     return [(distance < sumOfRadii), distance, sumOfRadii, dx, dy];
+  }
+
+  lerp (currentValue, targetValue, time) {
+    return currentValue * (1 - time) + targetValue * time;
   }
 
   createExplosionPool() {
@@ -150,8 +156,9 @@ export default class Game {
         return this.explosionPool[i];
       }
     }
-    
   }
+
+
 }
 
 let weatherChecks = document.querySelectorAll('.weather input');
