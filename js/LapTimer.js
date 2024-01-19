520ceea80586cbc4e0884f88b8949249e27d9e42
ikbensiep <ikbensiep>
2024-01-19 13:04:51 +0100

    WIP lap + sector timer

--- js/LapTimer.js
+++ js/LapTimer.js
@@ -1,36 +1,41 @@
 export default class LapTimer {
 
   constructor(player) {
-
     this.player = player;
     this.currentLap = 0;
-    this.laps = [{start: null, sectors: []}];
-
+    this.currentSector = 0;
+    this.laps = [{start: 0, sectors: []}];
   }
 
   startLap () {
     let thisLap = this.laps[this.currentLap];
-    
-    if(thisLap.start) { 
+
+    document.querySelector('.thislap').dataset.lap = this.currentLap;
+
+    // if lap has a start time, do nothing
+    if(thisLap.start !== 0) { 
+      console.warn(thisLap);
       return;
     } else {
-      console.log('startLap', thisLap)
-      
+
       let now = new Date();
       thisLap = {start: now.getTime(), sectors: []};
+
+      console.log('startLap', thisLap)
     }
 
   }
 
   setSectorTime () {
 
-    if(this.laps[this.currentLap] || 
-      this.laps[this.currentLap].sectors[this.player.currentSector]) return;
+    if(this.laps[this.currentLap]?.sectors[this.currentSector]) {
+        return;
+      }
     
     let currentTime = new Date();
     console.log('setSectorTime', this.player.currentSector)
 
-    this.laps[this.currentLap].sectors.push( {time: currentTime.getTime()} );
+    this.laps[this.currentLap].sectors.push( currentTime.getTime() );
 
     if(this.laps[this.currentLap].sectors.length == 3) {
       this.currentLap++;
@@ -40,9 +45,5 @@ export default class LapTimer {
       this.player.currentSector++
     }, 5000)
 
-
   }
-
-
-
 }
