89a5e38ca33ca5ebb7b069f7e1be0687131e9ee9
ikbensiep <ikbensiep>
2024-01-19 13:04:21 +0100

    WIP: Gain node to control engine volume

--- js/Sound.js
+++ js/Sound.js
@@ -3,56 +3,57 @@ export default class Sound {
 		this.url = options.url;
     this.loop = true;
     this.fadein = options.fadein;
+		this.playing = false;
 
-    this.AudioContext = window.AudioContext || window.webkitAudioContext
-    this.audioCtx = null
-    this.sourceBuffer = null
-    this.gainNode = null
-    this.gain = 0
-    this.interval = null
+    this.AudioContext = window.AudioContext || window.webkitAudioContext;
+    this.audioCtx = null;
+    this.sourceBuffer = null;
+    this.gainNode = null;
+    this.gain = options.gain;
+    this.interval = undefined;
 
     this.init();
   }
 
   init () {
 		this.audioCtx = new this.AudioContext();
+		this.sourceBuffer = this.audioCtx.createBufferSource();
 		this.gainNode = this.audioCtx.createGain();
+		
 		this.gainNode.connect(this.audioCtx.destination);
 		this.gainNode.gain.value = this.gain ? this.gain : this.fadein ? 0.0 : 0.05;
-		this.sourceBuffer = this.audioCtx.createBufferSource();
 		this.getSound();
-
+		console.log(`🔊 gain: ${this.gainNode.gain.value}`);
 	}
 
 	getSound () {
-
 		var url = this.url;
 		var request = new XMLHttpRequest();
 		request.open("GET", url, true);
 		request.responseType = 'arraybuffer';
 
 		request.onload = () => {
-
 			var undecodedAudio = request.response;
 			this.audioCtx.decodeAudioData(undecodedAudio, (buffer) => {
 
-			// Tell the AudioBufferSourceNode to use this AudioBuffer.
-			this.sourceBuffer.buffer = buffer;
-			this.sourceBuffer.connect(this.gainNode);
-			this.sourceBuffer.loop = this.loop;
-			this.sourceBuffer.playbackRate.value = 1;
+				// Tell the AudioBufferSourceNode to use this AudioBuffer.
+				this.sourceBuffer.connect(this.gainNode);
+				this.sourceBuffer.buffer = buffer;
+				this.sourceBuffer.loop = this.loop;
+				this.sourceBuffer.playbackRate.value = 1;
 
-			if( this.fadein) {
-				
-				this.interval = setInterval(() => { this.fadeIn()}, 100);
-			}
-
-			this.sourceBuffer.start(this.audioCtx.currentTime);
-			
+				if( this.fadein) {
+					this.interval = setInterval(() => { this.fadeIn()}, 100);
+				}
 			});
 		};
-
 		request.send();
+	}
+
+	updateGain (level) {
+		this.gainNode.gain.value = level;
+		console.log(this.gainNode.gain);
+		console.log(`🔊 gain: ${this.gainNode.gain.value}`);
 	}
 
 	fadeIn () {
@@ -60,23 +61,27 @@ export default class Sound {
 		if(this.gain < 0.25) {
 			this.gain += 0.002;
 			this.updateGain(this.gain);
-			
+			console.log(this.gain)
 		} else {
 			this.interval = clearInterval(this.interval);
-			
 		}
 	}
 
-	updateGain (level) {
-		this.gainNode.gain.value = level;
-	}
 
 	toggleLoop() {
 		this.sourceBuffer.loop = !this.sourceBuffer.loop;
 	}
 
+	startSound () {
+		if(!this.playing) {
+			this.sourceBuffer.start(0);
+			this.playing = true;
+		}
+	}
+
 	stopSound () {
 		this.sourceBuffer.stop();
+		this.playing = false
 	}
 
 }
